85e733d127344a102d5aacc5ea74083ca5e89eba
va1bhav-sh <vaibhavsharma.dev28@gmail.com>
2026-07-15 21:00:19 +0530

    Selection Sort

---
+++ 03-Sorting-Algortihms/02-Selection-Sort/insertion-sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int arr[] = {4, 1, 5, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    selectionSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
